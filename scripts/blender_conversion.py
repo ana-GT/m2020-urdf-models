2854a52e5dc1c7eeb1d3b1fa93cc0572de751552
Ana C. Huaman Quispe <ana@traclabs.com>
2023-04-26 10:33:19 -0500

    Updated script to handle glb and gtfi

--- scripts/blender_conversion.py
+++ scripts/blender_conversion.py
@@ -3,36 +3,30 @@
 import bpy
 import os
 
-path_ws="/home/ana/ros/plum"
+path_ws="/home/ana/ros/craftsman"
 path_rover= path_ws + "/src/m2020_urdf_models/rover/meshes"
 path_drone= path_ws + "/src/m2020_urdf_models/mhs/meshes"
 
-print("Converting meshes for the rover from GLTF to dae")
-for root, dirs, files in os.walk(path_rover):
-  for f in files:
-    if f.endswith('.gltf'):
-      gltf_file = os.path.join(path, f)
-      dae_file = os.path.splitext(gltf_file)[0] + ".dae"
+paths = [path_rover, path_drone]
+
+for path in paths:
+  print("Converting meshes from GLTF to dae from folder %s " % path)
+
+  for root, dirs, files in os.walk(path):
+    for f in files:
+      if f.endswith('.gltf') or f.endswith('.glb'):
+        gltf_file = os.path.join(path, f)
+        dae_file = os.path.splitext(gltf_file)[0] + ".dae"
       
-      bpy.ops.object.select_all(action='SELECT')
-      bpy.ops.object.delete()
+        bpy.ops.object.select_all(action='SELECT')
+        bpy.ops.object.delete()
+       
+        bpy.ops.import_scene.gltf(filepath=gltf_file)
+        bpy.ops.object.select_all(action='SELECT')
+        bpy.ops.wm.collada_export(filepath=dae_file, export_global_forward_selection='-Z', export_global_up_selection='Y', apply_global_orientation=True, selected=True)
+        
+  print("Finished converting meshes from folder %s " % path)
+  
+print("Finished running script!")   
       
-      bpy.ops.import_scene.gltf(filepath=gltf_file)
-      bpy.ops.object.select_all(action='SELECT')
-      bpy.ops.wm.collada_export(filepath=dae_file, export_global_forward_selection='-Z', export_global_up_selection='Y', apply_global_orientation=True, selected=True)
-      
-      
-print("Converting meshes for the drone from GLTF to dae")
-for root, dirs, files in os.walk(path_drone):
-  for f in files:
-    if f.endswith('.gltf'):
-      gltf_file = os.path.join(path, f)
-      dae_file = os.path.splitext(gltf_file)[0] + ".dae"
-      
-      bpy.ops.object.select_all(action='SELECT')
-      bpy.ops.object.delete()
-      
-      bpy.ops.import_scene.gltf(filepath=gltf_file)
-      bpy.ops.object.select_all(action='SELECT')
-      bpy.ops.wm.collada_export(filepath=dae_file, export_global_forward_selection='-Z', export_global_up_selection='Y', apply_global_orientation=True, selected=True)
-            
+
